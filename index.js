992f341e8fdc9ab1bc8159fcc92f58fb13f1b855
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2023-12-14 22:34:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(pickupLocation) {
-    const hqLocation = 42;
-    return Math.abs(pickupLocation - hqLocation);
+    const scuberHq = 42;
+    return Math.abs(pickupLocation - scuberHq);
 }
   function distanceFromHqInFeet(pickupLocation) {
     distanceFromHqInBlocks(pickupLocation);
-    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
     const feetPerBlock = 264
-    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
     const blocks = Math.abs(pickupLocation - 42);
-    // the return value of distanceFromHqInBlocks can then be used to calculate feet
     return feetPerBlock * blocks;
 }
 
   function distanceTravelledInFeet(start, destination) {
-    //returns the number of feet traveled
     const distanceTravelled = Math.abs(destination - start);
     const feetPerBlock = 264;
     return distanceTravelled * feetPerBlock;
